97faea18818e581da327455665938864988aedfb
Richard Spindler <richard@lateralblast.com.au>
2013-08-07 09:26:52 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+pdf2ocr
+=======
+
+Shell script to OCR PDFs. Has support for multipage documents.
+
+Usage
+=====
+
+	$ pdf2ocr file.pdf
+	OCRing file.pdf to file.txt
+
+Requirements
+============
+
+This script requires ghostscript and tesseract.
+
+Installing these on OS X can be done with brew:
+
+	$ brew install ghostscript
+	$ brew install tesseract
---
+++ pdf2ocr.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Name:         pdf2ocr
+# Version:      0.0.1
+# Release:      1
+# License:      Open Source
+# Group:        System
+# Source:       N/A
+# URL:          http://lateralblast.com.au/
+# Distribution: UNIX
+# Vendor:       Lateral Blast
+# Packager:     Richard Spindler <richard@lateralblast.com.au>
+# Description:  Shell script to OCR PDFs
+
+file_header=`basename "$1" .pdf`
+echo "OCRing $1 to $file_header.txt"
+out_file=$file_header-%02d.tiff
+gs -q -dNOPAUSE -dBATCH -dSAFER -r300x300 -sDEVICE=tiffg3 "-sOutputFile=$out_file" -c save pop -f "$1"
+cat /dev/null > $file_header.txt
+for tiff_file in $file_header*.tiff; do
+  text_file=`basename $tiff_file .tiff`
+  tesseract $tiff_file $text_file > /dev/null 2>&1
+  cat $text_file.txt >> $file_header.txt
+  rm $tiff_file $text_file.txt
+done
